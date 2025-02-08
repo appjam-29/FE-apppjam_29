95bdb66138c17886e3cc05399f481a622788f6fc
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2025-02-09 00:17:54 +0900

    feat: implement MarkerOverlay component and enhance map functionality with nearby places retrieval

---
+++ src/app/(main)/(recommend)/layout.tsx
@@ -0,0 +1,13 @@
+import { VStack } from '@tapie-kr/inspire-react';
+
+export default function RecommendLayouts({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <VStack fullWidth fullHeight>
+      {children}
+    </VStack>
+  );
+}
--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,25 +1,86 @@
 'use client';
 
+import { api } from '@/api/base';
 import BottomSheet from '@/components/BottomSheet';
 import KakaoMap from '@/components/KakaoMap';
+import MarkerOverlay from '@/components/MarkerOverlay';
 import PlaceCategory from '@/components/PlaceCategory';
 import { HStack, VStack } from '@tapie-kr/inspire-react';
 import { useRouter } from 'next/navigation';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { ZoomControl } from 'react-kakao-maps-sdk';
 
 export default function Home() {
   const router = useRouter();
+  const [pos, setPos] = useState({ lat: 37.545085, lng: 127.057695 });
+  const [places, setPlaces] = useState([]);
+
+  async function getNearybyPlaces(map: any) {
+    const response = await api(true).get(
+      `/places/nearby?latitude=${pos.lat}&longitude=${pos.lng}&radius=3&max_results=1000`,
+    );
+
+    setPlaces(
+      response.data.data.places.map((place: any) => {
+        return {
+          name: place.name,
+          lat: place.latitude,
+          lng: place.longitude,
+        };
+      }),
+    );
+  }
+
+  useEffect(() => {
+    console.log(places);
+  }, [places]);
 
   useEffect(() => {
     if (!localStorage.getItem('token')) {
       router.push('/login');
     }
+
+    api(true)
+      .get(
+        `/places/nearby?latitude=${pos.lat}&longitude=${pos.lng}&radius=0.5&max_result=1000`,
+      )
+      .then((response) => {
+        setPlaces(
+          response.data.data.places.map((place: any) => {
+            return {
+              name: place.name,
+              lat: place.latitude,
+              lng: place.longitude,
+            };
+          }),
+        );
+      });
   }, []);
 
   return (
     <VStack fullWidth fullHeight>
-      <KakaoMap center={{ lat: 37.5665, lng: 126.978 }} />
-      <BottomSheet height={500}>
+      <KakaoMap
+        center={{ lat: pos.lat, lng: pos.lng }}
+        onDragEnd={(map) => {
+          // 지도 드래그가 끝났을 때 호출되는 함수
+          const latlng = map.getCenter();
+
+          setPos({ lat: latlng.getLat(), lng: latlng.getLng() });
+
+          getNearybyPlaces(map);
+        }}
+        onZoomChanged={(map) => {
+          getNearybyPlaces(map);
+        }}>
+        <ZoomControl />
+        {places.map((place: any, index) => (
+          <MarkerOverlay
+            key={index}
+            position={{ lat: place.lat, lng: place.lng }}
+          />
+        ))}
+      </KakaoMap>
+      <BottomSheet height={300}>
         <HStack fullWidth>
           <PlaceCategory
             label={'작업하기 좋은 카페'}
