537c4955d341d37173be379c4ccb743634f58cf1
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2025-02-08 17:31:21 +0900

    enhance next.config.ts with SVG support and path aliasing

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,24 @@
-import { createVanillaExtractPlugin } from "@vanilla-extract/next-plugin";
+import { createVanillaExtractPlugin } from '@vanilla-extract/next-plugin';
+import path from 'path';
 
 const withVanillaExtract = createVanillaExtractPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  webpack: (config: any) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, 'src'),
+    };
+
+    return config;
+  },
+};
 
 export default withVanillaExtract(nextConfig);
